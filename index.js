dc5d8efa5504855d50ded5907c3f74ca94b739fc
Mayank Gupta <mayankgupta8995@gmail.com>
2021-04-18 07:36:01 +0530

    Header fixed

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const newLocal = "hello world";
 console.log(newLocal);
 
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".nav-links");
 function animateScroll(idToScrollTo) {
     console.log("idToScrollTo>>", idToScrollTo);
+    if (window.outerWidth <= 425)
+        linksContainer.style.height = 0;
     $("html, body").animate({ scrollTop: $('#' + idToScrollTo).offset().top - 50 }, 100);
 }
+
+
+navToggle.addEventListener("click", function () {
+    // linksContainer.classList.toggle("show-links");
+
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    console.log("linksHeight>>>", linksHeight, "containerHeight>>>", containerHeight);
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight + 5}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+    // console.log(linksContainer.getBoundingClientRect());
+});
