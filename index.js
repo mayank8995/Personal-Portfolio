083c0aa20c52ef7d553766885ca2df2f4bf08b2d
Mayank Gupta <mayankgupta8995@gmail.com>
2021-04-19 10:03:34 +0530

    Second version committed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function animateScroll(idToScrollTo) {
     console.log("idToScrollTo>>", idToScrollTo);
     if (window.outerWidth <= 425)
         linksContainer.style.height = 0;
-    $("html, body").animate({ scrollTop: $('#' + idToScrollTo).offset().top - 50 }, 100);
+    $("html, body").animate({ scrollTop: $('#' + idToScrollTo).offset().top - 60 }, 100);
 }
 
 
@@ -25,3 +25,60 @@ navToggle.addEventListener("click", function () {
     }
     // console.log(linksContainer.getBoundingClientRect());
 });
+
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+//Text animation:https://codepen.io/CheeseTurtle/pen/jzdgI
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
